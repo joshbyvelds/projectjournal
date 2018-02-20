749eae8eb95a6e1a790040cb7ee67de729618bfa
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-02-19 22:29:19 -0500

    Copied setup from bookmarker.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//Get all of our good Ol' composer stuff
+require_once("vendor/autoload.php");
+
+// Setup Twig cuz it's you know Awesome..
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/view');
+$twig = new \Twig_Environment($loader);
+
+//TODO:: Check if user is logged in..
+$loggedIn = false;
+
+//TODO:: Get user config if logged in else load default
+
+//TODO:: setup bookmarks
+
+// Finally lets render our templates..
+
+// Twig vars go here. :)
+echo $twig->render('index.twig',
+    [
+        'title' => 'Bookmarker!'
+    ]
+);
