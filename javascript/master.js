547ddd6415181af71318a8d10f9db41911bd4992
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-02-22 21:57:40 -0500

    setup typescript to use jquery type, created project save class and created favicon for project

--- javascript/master.js
+++ javascript/master.js
@@ -1,1 +1,15 @@
+var ProjectSaver = (function () {
+    function ProjectSaver() {
+        var _this = this;
+        $(document).ready(function () { _this.init(); });
+    }
+    ProjectSaver.prototype.save = function () {
+    };
+    ProjectSaver.prototype.reset = function () {
+    };
+    ProjectSaver.prototype.init = function () {
+    };
+    return ProjectSaver;
+}());
+var projectSaver = new ProjectSaver();
 //# sourceMappingURL=master.js.map
---
+++ typescript/master.ts
@@ -0,0 +1,20 @@
+class ProjectSaver {
+    //greeting: string;
+    constructor() {
+        $(document).ready(() => {this.init();});
+    }
+
+    private save() {
+
+    }
+
+    private reset() {
+
+    }
+
+    public init() {
+
+    }
+}
+
+let projectSaver = new ProjectSaver();
