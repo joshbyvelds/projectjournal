a750b0349f750053ba143fcaef105dd8a8f8cbd3
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-02-22 22:32:38 -0500

    created basic new project validation system, need to impleamment front-end error UI

--- javascript/master.js
+++ javascript/master.js
@@ -1,13 +1,38 @@
 var ProjectSaver = (function () {
     function ProjectSaver() {
         var _this = this;
+        this.savelock = false;
         $(document).ready(function () { _this.init(); });
     }
     ProjectSaver.prototype.save = function () {
+        if (this.validate() && !this.savelock) {
+            alert("Save To DB!");
+        }
+    };
+    ProjectSaver.prototype.validate = function () {
+        var isValid = true;
+        var title = $("#new_project_title");
+        var category = $("#new_project_category");
+        var description = $("#new_project_description");
+        if ($.trim(title.html()) === "") {
+            alert("Error Title");
+            isValid = false;
+        }
+        if (category.val() === "void") {
+            alert("Error Category");
+            isValid = false;
+        }
+        if ($.trim(description.html()) === "") {
+            alert("Error Description");
+            isValid = false;
+        }
+        return isValid;
     };
     ProjectSaver.prototype.reset = function () {
     };
     ProjectSaver.prototype.init = function () {
+        var _this = this;
+        $("#save_new_project_btn").off().on("click", function () { _this.save(); });
     };
     return ProjectSaver;
 }());
--- typescript/master.ts
+++ typescript/master.ts
@@ -1,19 +1,47 @@
 class ProjectSaver {
-    //greeting: string;
+    savelock: boolean;
+
     constructor() {
+        this.savelock = false;
         $(document).ready(() => {this.init();});
     }
 
-    private save() {
+    private save(): void{
+        if(this.validate() && !this.savelock){
+            alert("Save To DB!");
+        }
+    }
 
+    private validate(): boolean{
+        let isValid = true;
+        const title = $("#new_project_title");
+        const category = $("#new_project_category");
+        const description = $("#new_project_description");
+
+        if($.trim(title.html()) === ""){
+            alert("Error Title");
+            isValid = false;
+        }
+
+        if(category.val() === "void"){
+            alert("Error Category");
+            isValid = false;
+        }
+
+        if($.trim(description.html()) === ""){
+            alert("Error Description");
+            isValid = false;
+        }
+
+        return isValid;
     }
 
     private reset() {
 
     }
 
-    public init() {
-
+    private init(): void{
+        $("#save_new_project_btn").off().on("click", () => {this.save();});
     }
 }
 
