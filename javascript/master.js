63e3e79bcce9cd1f42cab86bd7d12b8cebdf4799
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2018-02-23 23:19:01 -0500

    Added custom dropdown for new project category and new project images. completed new project validation (javascript)

--- javascript/master.js
+++ javascript/master.js
@@ -3,7 +3,7 @@ var ProjectSaver = (function () {
         var _this = this;
         this.savelock = false;
         this.title = $("#new_project_title");
-        this.category = $("#new_project_category");
+        this.category = $("#selected_category");
         this.description = $("#new_project_description");
         $(document).ready(function () { _this.init(); });
     }
@@ -15,15 +15,15 @@ var ProjectSaver = (function () {
     ProjectSaver.prototype.validate = function () {
         var _this = this;
         var isValid = true;
-        if ($.trim(this.title.html()) === "") {
+        if ($.trim(this.title.html()) === "" || $.trim(this.title.html()) === "New Project") {
             this.title.addClass("error").html("Please give the new project a title.").one("focus", function () { _this.title.removeClass("error").html(""); });
             isValid = false;
         }
-        if (this.category.val() === "void") {
-            alert("Error Category");
+        if ($.trim(this.category.html()) === "Please select") {
+            this.category.addClass("error");
             isValid = false;
         }
-        if ($.trim(this.description.html()) === "") {
+        if ($.trim(this.description.html()) === "" || $.trim(this.description.html()) === "description here...") {
             this.description.addClass("error").html("Please give the new project a description.").one("focus", function () { _this.description.removeClass("error").html(""); });
             isValid = false;
         }
@@ -33,9 +33,17 @@ var ProjectSaver = (function () {
     };
     ProjectSaver.prototype.reset = function () {
     };
+    ProjectSaver.prototype.openDropdown = function () {
+        $("#category_select .options").slideDown(500);
+        $("#category_select .option").off().on('click', function () {
+            $("#selected_category").html($(this).html()).removeClass("error");
+            $("#category_select .options").slideUp(500);
+        });
+    };
     ProjectSaver.prototype.init = function () {
         var _this = this;
         $("#save_new_project_btn").off().on("click", function () { _this.save(); });
+        $("#selected_category").on("click", function () { _this.openDropdown(); });
     };
     return ProjectSaver;
 }());
--- typescript/master.ts
+++ typescript/master.ts
@@ -7,7 +7,7 @@ class ProjectSaver {
     constructor() {
         this.savelock = false;
         this.title = $("#new_project_title");
-        this.category = $("#new_project_category");
+        this.category = $("#selected_category");
         this.description = $("#new_project_description");
         $(document).ready(() => {this.init();});
     }
@@ -21,17 +21,17 @@ class ProjectSaver {
     private validate(): boolean{
         let isValid = true;
 
-        if($.trim(this.title.html()) === ""){
+        if($.trim(this.title.html()) === "" || $.trim(this.title.html()) === "New Project"){
            this.title.addClass("error").html("Please give the new project a title.").one("focus", () =>{this.title.removeClass("error").html("");});
             isValid = false;
         }
 
-        if(this.category.val() === "void"){
-            alert("Error Category");
+        if($.trim(this.category.html()) === "Please select"){
+            this.category.addClass("error");
             isValid = false;
         }
 
-        if($.trim(this.description.html()) === ""){
+        if($.trim(this.description.html()) === "" || $.trim(this.description.html()) === "description here..."){
             this.description.addClass("error").html("Please give the new project a description.").one("focus", () =>{this.description.removeClass("error").html("");});
             isValid = false;
         }
@@ -47,8 +47,17 @@ class ProjectSaver {
 
     }
 
+    private openDropdown(){
+        $("#category_select .options").slideDown(500);
+        $("#category_select .option").off().on('click', function() {
+            $("#selected_category").html($(this).html()).removeClass("error");
+            $("#category_select .options").slideUp(500);
+        });
+    }
+
     private init(): void{
         $("#save_new_project_btn").off().on("click", () => {this.save();});
+        $("#selected_category").on("click", () => {this.openDropdown();})
     }
 }
 
