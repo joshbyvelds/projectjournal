00870453c30ca062dd097bdf1a847ddc94e66d1a
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2019-02-09 20:05:28 -0500

    Added config files for behat and phpspec

--- production/index.php
+++ production/index.php
@@ -2,7 +2,7 @@
 
 require_once '../vendor/autoload.php';
 
-$loader = new \Twig_Loader_Filesystem(__DIR__.'/view');
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/views');
 $twig = new \Twig_Environment($loader);
 
 echo $twig->render('index.twig');
