1a9f28b0efa9e3e9e0141a538929f98a8dfe624d
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2019-02-09 21:20:41 -0500

    Created Basic Router

--- production/index.php
+++ production/index.php
@@ -1,8 +1,30 @@
 <?php
 
 require_once '../vendor/autoload.php';
+require_once 'php/ProjectJournal/Services/Router.php';
 
+// Setup Twig..
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/views');
 $twig = new \Twig_Environment($loader);
+
+// Setup Router..
+$router = new ProjectJournal\Services\Router();
+$current_route = $_SERVER['REQUEST_URI'];
+
+// TODO:: Move routes to config folder..
+$router->route('/', function(){
+    return 'this is the homepage';
+});
+
+$router->route('/projects', function(){
+    return 'this is the projects page';
+});
+
+//TODO:: check if route is in the config folder.. if not, throw 404..
+http_response_code(404);
+echo $twig->render('404.twig');
+die();
+
+$router->dispatch($current_route);
 
 echo $twig->render('index.twig');
---
+++ production/php/ProjectJournal/Services/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjectJournal\Services;
+
+class Router
+{
+
+    private $routes;
+
+    // TODO:: Delete constructor this if not used later..
+    public function __construct()
+    {
+
+    }
+
+    public function route($action, $callback)
+    {
+        $action = trim($action, "/");
+        $this->routes[$action] = $callback;
+    }
+
+    public function dispatch($action)
+    {
+        $action = trim($action, "/");
+        $callback = $this->routes[$action];
+        echo call_user_func($callback);
+    }
+}
