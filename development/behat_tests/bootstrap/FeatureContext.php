d13ad7ebd293b0ff7438af54c8b68ff365b21ce4
Josh Byvelds <joshbyvelds@users.noreply.github.com>
2019-08-05 18:17:13 -0400

    Updated Chromedriver and add init behat test

--- development/behat_tests/bootstrap/FeatureContext.php
+++ development/behat_tests/bootstrap/FeatureContext.php
@@ -3,6 +3,8 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +20,44 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @When I wait for :text to appear
+     * @Then I should see :text appear
+     * @param $text
+     * @throws \Exception
+     */
+    public function iWaitForTextToAppear($text)
+    {
+        $this->spin(function(FeatureContext $context) use ($text) {
+            try {
+                $context->assertPageContainsText($text);
+                return true;
+            }
+            catch(ResponseTextException $e) {
+                // NOOP
+            }
+            return false;
+        });
+    }
+    public function spin ($lambda, $wait = 60)
+    {
+        for ($i = 0; $i < $wait; $i++)
+        {
+            try {
+                if ($lambda($this)) {
+                    return true;
+                }
+            } catch (Exception $e) {
+                // do nothing
+            }
+            sleep(1);
+        }
+        $backtrace = debug_backtrace();
+        throw new Exception(
+            "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
+            $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
+        );
     }
 }
